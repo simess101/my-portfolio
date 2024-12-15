c30f787750fd4a24c3d832fa66415b6a7bdbc67b
Shane <simess@vcu.edu>
2024-12-15 01:08:41 -0500

    Preparing for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/', // Ensure this matches your repository name
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './public/assets'),
+    },
+  },
 });
